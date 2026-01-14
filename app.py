0e6b9e4e35fb3b62383c70a1f4f7c18a14f9f42f
jmsksks6-source <jmsksks6@gmail.com>
2026-01-14 19:13:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,51 +1,52 @@
-import os
-from flask import Flask, render_template
+import os, random
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit, join_room
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-rooms = {} # {room_id: {players: [], turn: 0, properties: {}}}
+rooms = {} 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @socketio.on('create_room')
-def create(data):
+def on_create(data):
     room = data['room']
-    rooms[room] = {'players': [], 'turn': 0, 'properties': {}}
-    join_room(room)
-    emit('room_created', {'room': room})
+    if room not in rooms:
+        rooms[room] = {'players': [], 'turn': 0, 'properties': {}}
+        emit('status', {'msg': f'تم إنشاء الغرفة {room} بنجاح سيدي', 'success': True})
+    else:
+        emit('status', {'msg': 'الغرفة موجودة بالفعل سيدي', 'success': True})
 
 @socketio.on('join_game')
-def join(data):
+def on_join(data):
     room = data['room']
     name = data['name']
     if room in rooms:
         join_room(room)
-        rooms[room]['players'].append({'name': name, 'money': 1500, 'pos': 0, 'sid': data['sid']})
-        emit('update_game', rooms[room], room=room)
+        # منع تكرار نفس اللاعب
+        player_data = {'name': name, 'money': 1500, 'pos': 0, 'id': request.sid, 'color': data['color']}
+        rooms[room]['players'].append(player_data)
+        # إرسال التحديث لكل من في الغرفة حصراً
+        emit('update_game', rooms[room], to=room)
+        emit('join_success', {'success': True})
+    else:
+        emit('join_success', {'success': False, 'msg': 'الغرفة غير موجودة، قم بإنشائها أولاً سيدي'})
 
 @socketio.on('roll_dice')
-def roll(data):
+def on_roll(data):
     room = data['room']
-    r_data = rooms[room]
-    player_idx = r_data['turn']
-    
-    import random
-    steps = random.randint(1, 6)
-    player = r_data['players'][player_idx]
-    player['pos'] = (player['pos'] + steps) % 24 # خريطة أكبر
+    if room in rooms:
+        game = rooms[room]
+        current_player = game['players'][game['turn']]
+        if current_player['id'] == request.sid:
+            steps = random.randint(1, 6)
+            current_player['pos'] = (current_player['pos'] + steps) % 16
+            game['turn'] = (game['turn'] + 1) % len(game['players'])
+            emit('dice_result', {'steps': steps, 'game': game, 'roller': current_player['name']}, to=room)
 
-    # تعاقب الدور
-    r_data['turn'] = (player_idx + 1) % len(r_data['players'])
-    emit('dice_result', {'steps': steps, 'game': r_data}, room=room)
-
-@socketio.on('buy_property')
-def buy(data):
-    room = data['room']
-    prop_id = data['prop_id']
-    player_name = data['name']
-    # منطق الخصم والامتلاك
-    # ... (سنكمله في التحديث القادم)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 10000))
+    socketio.run(app, host='0.0.0.0', port=port)

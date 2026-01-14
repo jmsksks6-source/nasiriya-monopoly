d4b1b36224b1932debda5f127588e6e144917ee0
jmsksks6-source <jmsksks6@gmail.com>
2026-01-14 18:57:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,51 @@
 import os
 from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
-import random
+from flask_socketio import SocketIO, emit, join_room
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-players = {}
+rooms = {} # {room_id: {players: [], turn: 0, properties: {}}}
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@socketio.on('join')
-def on_join(data):
+@socketio.on('create_room')
+def create(data):
+    room = data['room']
+    rooms[room] = {'players': [], 'turn': 0, 'properties': {}}
+    join_room(room)
+    emit('room_created', {'room': room})
+
+@socketio.on('join_game')
+def join(data):
+    room = data['room']
     name = data['name']
-    players[name] = {"pos": 0, "money": 1500, "color": data['color']}
-    emit('update_players', players, broadcast=True)
+    if room in rooms:
+        join_room(room)
+        rooms[room]['players'].append({'name': name, 'money': 1500, 'pos': 0, 'sid': data['sid']})
+        emit('update_game', rooms[room], room=room)
 
 @socketio.on('roll_dice')
-def handle_roll(data):
-    name = data['name']
-    dice_val = random.randint(1, 6)
-    players[name]['pos'] = (players[name]['pos'] + dice_val) % 16
-    # نرسل نتيجة الزار واسم اللاعب لكل الموجودين
-    emit('dice_rolled', {'name': name, 'value': dice_val, 'players': players}, broadcast=True)
+def roll(data):
+    room = data['room']
+    r_data = rooms[room]
+    player_idx = r_data['turn']
+    
+    import random
+    steps = random.randint(1, 6)
+    player = r_data['players'][player_idx]
+    player['pos'] = (player['pos'] + steps) % 24 # خريطة أكبر
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))
-    socketio.run(app, host='0.0.0.0', port=port)
+    # تعاقب الدور
+    r_data['turn'] = (player_idx + 1) % len(r_data['players'])
+    emit('dice_result', {'steps': steps, 'game': r_data}, room=room)
+
+@socketio.on('buy_property')
+def buy(data):
+    room = data['room']
+    prop_id = data['prop_id']
+    player_name = data['name']
+    # منطق الخصم والامتلاك
+    # ... (سنكمله في التحديث القادم)

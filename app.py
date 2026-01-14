0eb73c34ff09a35b9f46afb33da28bc5160f496e
jmsksks6-source <jmsksks6@gmail.com>
2026-01-14 19:44:45 +0300

    Update app.py

--- app.py
+++ app.py
@@ -15,32 +15,20 @@ def index():
 def on_create(data):
     room = data['room']
     if room not in rooms:
-        rooms[room] = {'players': [], 'turn': 0, 'properties': {}} # properties: {pos: owner_name}
+        rooms[room] = {'players': [], 'turn': 0, 'properties': {}}
         join_room(room)
-        emit('status', {'msg': 'تم إنشاء الغرفة بنجاح سيدي', 'success': True})
+        emit('status', {'msg': 'تم إنشاء الغرفة سيدي', 'success': True})
 
 @socketio.on('join_game')
 def on_join(data):
     room = data['room']
     if room in rooms:
         join_room(room)
-        player_data = {'name': data['name'], 'money': 1500, 'pos': 0, 'id': request.sid, 'color': data['color']}
+        # موازنة الرصيد الابتدائي سيدي
+        player_data = {'name': data['name'], 'money': 2000, 'pos': 0, 'id': request.sid, 'color': data['color']}
         rooms[room]['players'].append(player_data)
         emit('update_game', rooms[room], to=room)
         emit('join_success', {'success': True})
-
-@socketio.on('buy_land')
-def on_buy(data):
-    room = data['room']
-    game = rooms[room]
-    player = next(p for p in game['players'] if p['name'] == data['name'])
-    price = data['price']
-    
-    if player['money'] >= price:
-        player['money'] -= price
-        game['properties'][str(player['pos'])] = player['name']
-        emit('update_game', game, to=room)
-        emit('log', {'msg': f'🏦 {player["name"]} اشترى {data["land_name"]}'}, to=room)
 
 @socketio.on('roll_dice')
 def on_roll(data):
@@ -49,10 +37,30 @@ def on_roll(data):
     player = game['players'][game['turn']]
     if player['id'] == request.sid:
         steps = random.randint(1, 6)
-        player['pos'] = (player['pos'] + steps) % 20 # خريطة أكبر 20 منطقة
+        old_pos = player['pos']
+        player['pos'] = (player['pos'] + steps) % 40 # 40 منطقة
+        
+        # منطق الفرة الكاملة (راتب العبور)
+        if player['pos'] < old_pos:
+            player['money'] += 200
+            emit('log', {'msg': f'💰 {player["name"]} عبر البداية واستلم 200$'}, to=room)
+
         game['turn'] = (game['turn'] + 1) % len(game['players'])
         emit('dice_result', {'steps': steps, 'game': game, 'roller': player['name']}, to=room)
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))
-    socketio.run(app, host='0.0.0.0', port=port)
+@socketio.on('update_money') # لتحديث الرصيد من الصندوق
+def on_money(data):
+    room = data['room']
+    player = next(p for p in rooms[room]['players'] if p['name'] == data['name'])
+    player['money'] += data['amount']
+    emit('update_game', rooms[room], to=room)
+
+@socketio.on('buy_land')
+def on_buy(data):
+    room = data['room']
+    game = rooms[room]
+    player = next(p for p in game['players'] if p['name'] == data['name'])
+    if player['money'] >= data['price']:
+        player['money'] -= data['price']
+        game['properties'][str(player['pos'])] = player['name']
+        emit('update_game', game, to=room)
